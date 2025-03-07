a0c60fe4915f1e3f5db4987e51408f7e6db01af7
Your Name <t.sibo@alustudent.com>
2025-03-06 23:51:48 -0800

    more data structures

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+# adds all unique integers in a list
+
+
+def uniq_add(my_list=[]):
+    sum_us = 0
+    temp_list = []
+    for i in my_list:
+        if i not in temp_list:
+            temp_list.append(i)
+    for temp in temp_list:
+        sum_us += temp
+    return sum_us
